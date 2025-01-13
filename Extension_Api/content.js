517ca048bbdc9bb150e30c4061b2e7f1db1f5e4a
Koray Renkligil <koray.renkligill@gmail.com>
2025-01-13 13:47:49 +0300

    login test 4

--- Extension_Api/content.js
+++ Extension_Api/content.js
@@ -171,9 +171,7 @@ $(document).ready(function () {
   ) {
     activeLanguage = localStorage.getItem("activeLanguage");
   }
-
   var date = new Date().valueOf();
-  //TODO: AÇ!
   fetch(
     "https://extensions-bucket.fra1.cdn.digitaloceanspaces.com/content.css?v=" +
       date
@@ -199,14 +197,18 @@ $(document).ready(function () {
       language = json;
     })
     .then(function () {
+      console.log("extension toollarını aç");
       createExtensionTools();
     });
+
   checkLogin();
 });
 
 function createExtensionTools() {
+  console.log("createExtensionTools");
   $(document).ajaxError(function (event, jqXHR) {
     if (jqXHR.status === 401) {
+      console.log("createExtensionTools 401");
       // Redirect the user to the login page
       signOut();
       location.reload();
@@ -215,11 +217,14 @@ function createExtensionTools() {
   });
 
   if (user != null && user.token.length > 0) {
+    console.log("user var");
   } else {
+    console.log("user yok");
     createSellerFlashMenu(false);
     return;
   }
 
+  console.log("user var 2");
   createSellerFlashMenu(true);
   $("#sf-logout").click(function () {
     signOut();
